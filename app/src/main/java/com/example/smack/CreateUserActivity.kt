410e4b8622e6a1459fb0de7c9d4651070e597a62
paul-romeo <countmedream@yahoo.com>
2020-10-03 20:27:50 -0700

    lesson 4

---
+++ app/src/main/java/com/example/smack/CreateUserActivity.kt
@@ -0,0 +1,16 @@
+package com.example.smack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    fun generateUserAvatar(view: View) {}
+    fun createUserBtnClicked(view: View) {}
+    fun generateColorClicked(view: View) {}
+}
